96c37be6cbeb70a2e194770085bc8ddd7fc1b0a4
jcarias045 <jaime.carias@gmail.com>
2021-03-30 11:30:00 -0600

    cambios

--- app/models/company.model.js
+++ app/models/company.model.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-module.exports = (sequelize, Sequelize) => {
-	const Company = sequelize.define('sys_company', {	
-	  ID_Company: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-    },
-	  Name: {
-			type: Sequelize.STRING,
-      unique: true
-      },
-      Logo:{
-        type: Sequelize.STRING
-      },
-      Web:{
-        type: Sequelize.STRING
-      },
-      ShortName:{
-        type: Sequelize.STRING
-      },
-      Active:{
-          type: Sequelize.BOOLEAN
-      },
-      AccessToCustomers:{
-        type: Sequelize.BOOLEAN
-      },
-      AccessToSuppliers:{
-        type: Sequelize.BOOLEAN
-      },
-      RequiredIncome:{
-        type: Sequelize.BOOLEAN
-      },
-      RequiredOutput:{
-        type: Sequelize.BOOLEAN
-      },
-      CompanyRecords:{
-        type: Sequelize.BOOLEAN
-      },
-      AverageCost:{
-        type: Sequelize.BOOLEAN
-      }   
-	},{ //colocamos este parametro para que SEQUELIZE nos deje colocar el nombre del modelo y NO lo coloque plural
-        freezeTableName: true,
-        timestamps: false,
-	  });
-=======
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
@@ -123,7 +76,6 @@ module.exports = mongoose.model('Company', CompanySchema)
 //         freezeTableName: true,
 //         timestamps: false,
 // 	  });
->>>>>>> mongodb
 	
 // 	return Company;
 // }
