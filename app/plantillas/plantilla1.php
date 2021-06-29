b4c612630ab650a38edf92b23a3e25794aaf0124
Jaime Carias <jaime.carias@gmail.com>
2021-06-29 10:36:59 -0600

    cambios pdf html-pdf

--- app/plantillas/plantilla1.php
+++ app/plantillas/plantilla1.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title><?php echo "Cotizacion" ?></title>
-    </head>
+
+ 
     <style>
         h1 {
         font-size: 40px;
@@ -24,7 +19,7 @@
         }
 </style>
     <body>
-       
+        <?php echo "
          <div class='container-fluid'>
          <div class='row'>
              <table>
@@ -114,11 +109,11 @@
          </div>
       
      </div>
-    
-       
-        <!-- <footer>
-            <small> {{saludo}} </small>
-        </footer> -->
+    "?>
+<!--        
+        <footer>
+            <small> </small>
+        </footer>
     </body>
     
-    </html>
+    </html> -->
