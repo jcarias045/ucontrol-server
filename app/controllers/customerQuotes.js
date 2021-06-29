1318260a559abe2d287ed9fec8e5a49838f42430
Jaime Carias <jaime.carias@gmail.com>
2021-06-29 11:26:07 -0600

    cambios pdf html-pdf

--- app/controllers/customerQuotes.js
+++ app/controllers/customerQuotes.js
@@ -710,8 +710,131 @@ async function ImprimirCotizacionPDF(req,res){
     let img = "https://ucontrolv1.herokuapp.com/api/get-logo/" + logo;
     
     const ubicacionPlantilla = require.resolve("../../public/plantilla1.html");
-    let contenidoHtml = fs.readFileSync(ubicacionPlantilla, 'utf8')
-        
+    // let contenidoHtml = fs.readFileSync(ubicacionPlantilla, 'utf8')
+    //     console.log("CONTENIDI", contenidoHtml);
+    let contenidoHtml=`<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title><?php echo "Cotizacion" ?></title>
+    </head>
+    <style>
+        h1 {
+        font-size: 40px;
+        }
+
+        .empresa {
+        font-size: 10px;
+        }
+
+        p {
+        font-size: 14px;
+        }
+</style>
+    <body>
+        <?php echo "
+         <div class='container-fluid'>
+         <div class='row'>
+             <table>
+                 <tr>
+                     <td> {{logo}}</td>
+
+                     <td style='text-align:right' width='400px'>
+                         <div class='col'
+                             <p class='empresa'> {{nombreEmpresa}} </p>
+                             <p class='empresa'> {{direccionEmpresa}} </p>
+                             <p class='empresa'> {{webEmpresa}} </p>
+
+                     </td>
+                 </tr>
+             </table>
+
+
+
+         </div>
+         <div class='row'>
+             <table>
+                 <tr>
+                     <td> <small>Fecha</small> <h6> {{fecha}}</h6></td>
+                     <td width='250px'> </td>
+                     <td style='text-align:right' width='450px'>
+                         <div class='col'
+                         <small>Cotización</small>
+                         <h1 class='h2'> # {{codigo}} </h1>
+
+                     </td>
+                 </tr>
+             </table>
+
+         </div>
+
+         <hr>
+         <div class='row'>
+             <div class='col-10'>
+             <strong>Para:</strong>    <h1 class='h6'> {{para}} </h1>
+             <strong>Cliente:</strong>    <h1 class='h6'> {{cliente}} </h1>
+             <strong>Dirección:</strong>    <h1 class='h6'> {{direccion}} </h1>
+             <strong>Correo:</strong>    <h1 class='h6'> {{correo}} </h1>
+             </div>
+
+         </div>
+         <hr>
+
+         <div class='row'>
+             <div class='col-xs-12'>
+                 <table  class='table table-hover'>
+                     <thead  class='thead-dark'>
+                         <tr>
+                             <th>Cantidad</th>
+                             <th>Nombre</th>
+                             <th>Medida</th>
+                             <th>Precio</th>
+                             <th>Total</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         {{tablaProductos}}
+                     </tbody>
+                     <tfoot>
+                         <tr>
+                             <td colspan='3' class='text-right'>
+                                 <h4>Subtotal</h4>
+                             </td>
+                             <td>
+                                 <h4>{{subtotal}}</h4>
+                             </td>
+                         </tr>
+                         <tr>
+                             <td colspan='3' class='text-right' ></td>
+                             <td>{{impuestos}}</td>
+                         </tr>
+                         <tr>
+                             <td colspan='3' class='text-right'>
+                                 <h4>Total</h4>
+                             </td>
+                             <td>
+                                 <h4>{{total}}</h4>
+                             </td>
+                         </tr>
+                     </tfoot>
+                 </table>
+             </div>
+         </div>
+
+     </div>
+    "?>
+
+        <!-- <footer>
+            <small> {{saludo}} </small>
+        </footer> -->
+    </body>
+
+    </html>`
         //se busca la informacion de la cotizacion (header)
     let cotizacion = await CustomerQuote.findOne({ _id: req.params.id })
     .populate({ path: 'Customer', model: 'Customer', populate: { path: 'Sector', model: 'Sector' } })
